0c0514d59267f5261ad1b9f5d5478e26e59bd2f4
amandabzcosta <131314762+amandabzcosta@users.noreply.github.com>
2025-06-29 13:06:48 -0300

    Update 7.3.7 - Consulta professores de um departamento.sql

--- scripts/7.3.7 - Consulta professores de um departamento.sql
+++ scripts/7.3.7 - Consulta professores de um departamento.sql
@@ -1,10 +1,6 @@
--- ==================================================================================
 -- 7.3 - Consulta professores de um departamento.sql
 -- Objetivo: Listar os professores de um departamento específico.
--- Correções:
--- 1. A consulta original era impossível de ser executada no novo esquema, pois não há uma tabela ligando professores a disciplinas.
--- 2. A consulta foi simplificada para responder à pergunta de forma direta, usando a relação que existe: listar todos os professores que pertencem a um departamento.
--- ==================================================================================
+
 SELECT
     dep.Nome AS Nome_Departamento,
     u.Nome AS Nome_Professor,
