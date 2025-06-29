620b155cb4a91bcfab7bd513ba990aecbd9e8fec
amandabzcosta <131314762+amandabzcosta@users.noreply.github.com>
2025-06-29 13:01:34 -0300

    Create 7.3.6 - Consulta média geral de um aluno.sql

---
+++ scripts/7.3.6 - Consulta média geral de um aluno.sql
@@ -0,0 +1,20 @@
+-- ==================================================================================
+-- 7.3 - Consulta média geral de um aluno.sql
+-- Objetivo: Calcular a média geral das notas de um aluno específico.
+-- Correções:
+-- 1. A tabela 'Desempenho' foi substituída pela tabela 'Matriculas', que agora contém a coluna 'Nota'.
+-- ==================================================================================
+SELECT
+    u.Nome,
+    u.Sobrenome,
+    AVG(m.Nota) AS Media_Geral
+FROM
+    Usuario u
+JOIN
+    Aluno a ON u.ID_Usuario = a.ID_Usuario
+JOIN
+    Matriculas m ON a.ID_Usuario = m.ID_Aluno
+WHERE
+    u.ID_Usuario = 42
+GROUP BY
+    u.ID_Usuario, u.Nome, u.Sobrenome;
